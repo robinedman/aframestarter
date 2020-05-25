596ec606f4409db94ed6163f3d1d85d6bfc36a1b
“Robin <robin.edman@gmail.com>
2020-05-25 19:50:44 +0100

    Aframe starter

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Clone this repository.
+2. `npm install` to install dependencies.
+3. `npm start` to start.
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "aframe";
+import "aframe-particle-system-component";
+import "aframe-extras.ocean";
+import "aframe-gradient-sky";
+
+// Put code here
